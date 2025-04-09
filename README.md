98f9ca106cecb14a65dd0148fcd782dc481a6b39
chrisayling <chris.ayling.360@gmail.com>
2025-04-09 13:29:21 +1000

    readme + fmt

--- README.md
+++ README.md
@@ -1,19 +1,37 @@
 # IsaacSim Typings
 
-This repo contains stub files generated from NVIDIA's Isaacsim for use with vscode to enable type completion / checking
+Supports Isaac Sim 4.5.0
+
+This repo contains unofficial generated stub files generated from NVIDIA's Isaacsim for use with VSCode/Cursor to enable type completion / checking and AI assitance.
+
+> NVIDIA Isaac Sim™ is a reference application built on NVIDIA Omniverse that enables developers to develop, simulate, and test AI-driven robots in physically-based virtual environments.
+
+![Example](./assets/checking-completion-llm.png "Screenshot of Cursor editor demonstrating IsaacSim type support: left side shows type checking and import completion hints, right side shows an AI assistant leveraging the type information from the provided stubs")
+
+Documentation for Isaac Sim itself: [Installation and Guides](https://docs.isaacsim.omniverse.nvidia.com/4.5.0/index.html), [API Documentation](https://docs.isaacsim.omniverse.nvidia.com/4.5.0/py/index.html)
+
 
 ## Usage
 
-Clone this repo and configure your vscode to use the supplied files as stubs.
+For the type stubs in this repo to be useful, VSCode/Cursor needs to be able to find them.
+`
+By default, VSCode/Cursor will look in the `./typings` folder of your project for stubs. Therefore the easiest way to use the type stubs provided here is to copy them into the `./typings` folder in your project. You may choose to add `./typings` to your `.gitignore` file.
 
-https://code.visualstudio.com/docs/python/settings-reference
+Alternatively, you can customise where VSCode/Cursor looks for type stubs. See [settings.json](./.vscode/settings.json) for guidance on how to change this as well as https://code.visualstudio.com/docs/python/settings-reference.
+
+You will also need to disable VSCode/Cursor's warnings about missing source files by adding the following to your editor/project/workspace `settings.json` file. We need to this because we don't have access to the module source because the stubs are generated from compiled python extensions.
+
+```json
+"python.analysis.diagnosticSeverityOverrides": {
+    "reportMissingModuleSource": "none"
+}
+```
 
 ## Generating Stubs
 
 If you are only looking to use the provided stub files, then you can ignore this section.
 
-These commands will install `pybind11-stubgen` into your isaacsim python environment and then
-
+These commands will install `pybind11-stubgen` into your isaacsim python environment and then regenerate the stubs. This assumes you have installed Isaac Sim using the [Workstation Installation Instructions](https://docs.isaacsim.omniverse.nvidia.com/4.5.0/installation/install_workstation.html), other installation methods may install Isaac Sim to a different location.
 
 ```bash
 ~/isaacsim/python.sh -m pip install pybind11-stubgen
--- test/drop_example.py
+++ test/drop_example.py
@@ -12,23 +12,72 @@ from isaacsim.sensors.camera import Camera
 import isaacsim.core.utils.numpy.rotations as rot_utils
 
 world = World()
-world.scene.add_default_ground_plane()
+world.scene.add_default_ground_plane()  # type: ignore
 
-camera = Camera(prim_path="/World/Camera", name="camera", position=(0, 0, 5), resolution=(640, 480),
+camera = Camera(
+    prim_path="/World/Camera",
+    name="camera",
+    position=(0, 0, 5),
+    resolution=(640, 480),
     orientation=rot_utils.euler_angles_to_quats(np.array([0, 90, 0]), degrees=True)
-                )
+)
 
-cube1 = DynamicCuboid(prim_path="/World/Cube1", name="cube", position=(0, 0, 5), color=np.array([255, 0, 0]))
-cube2 = DynamicCuboid(prim_path="/World/Cube2", name="cube", position=(0, 0.5, 6), color=np.array([0, 255, 0]))
-cube3 = DynamicCuboid(prim_path="/World/Cube3", name="cube", position=(0, 1, 7), color=np.array([0, 0, 255]))
+cube1 = DynamicCuboid(
+    prim_path="/World/Cube1",
+    name="cube",
+    position=(0, 0, 5),
+    color=np.array([255, 0, 0]),
+)
+cube2 = DynamicCuboid(
+    prim_path="/World/Cube2",
+    name="cube",
+    position=(0, 0.5, 6),
+    color=np.array([0, 255, 0]),
+)
+cube3 = DynamicCuboid(
+    prim_path="/World/Cube3",
+    name="cube",
+    position=(0, 1, 7),
+    color=np.array([0, 0, 255]),
+)
 
-cone1 = DynamicCone(prim_path="/World/Cone1", name="cone", position=(0, 0, 8), color=np.array([255, 0, 0]))
-cone2 = DynamicCone(prim_path="/World/Cone2", name="cone", position=(0, -0.5, 9), color=np.array([0, 255, 0]))
-cone3 = DynamicCone(prim_path="/World/Cone3", name="cone", position=(0, -1, 10), color=np.array([0, 0, 255]))
+cone1 = DynamicCone(
+    prim_path="/World/Cone1",
+    name="cone",
+    position=(0, 0, 8),
+    color=np.array([255, 0, 0]),
+)
+cone2 = DynamicCone(
+    prim_path="/World/Cone2",
+    name="cone",
+    position=(0, -0.5, 9),
+    color=np.array([0, 255, 0]),
+)
+cone3 = DynamicCone(
+    prim_path="/World/Cone3",
+    name="cone",
+    position=(0, -1, 10),
+    color=np.array([0, 0, 255]),
+)
 
-cylinder1 = DynamicCylinder(prim_path="/World/Cylinder1", name="cylinder", position=(0, 0, 11), color=np.array([255, 0, 0]))
-cylinder2 = DynamicCylinder(prim_path="/World/Cylinder2", name="cylinder", position=(0.5, 0, 12), color=np.array([0, 255, 0]))
-cylinder3 = DynamicCylinder(prim_path="/World/Cylinder3", name="cylinder", position=(1, 0, 13), color=np.array([0, 0, 255]))
+cylinder1 = DynamicCylinder(
+    prim_path="/World/Cylinder1",
+    name="cylinder",
+    position=(0, 0, 11),
+    color=np.array([255, 0, 0]),
+)
+cylinder2 = DynamicCylinder(
+    prim_path="/World/Cylinder2",
+    name="cylinder",
+    position=(0.5, 0, 12),
+    color=np.array([0, 255, 0]),
+)
+cylinder3 = DynamicCylinder(
+    prim_path="/World/Cylinder3",
+    name="cylinder",
+    position=(1, 0, 13),
+    color=np.array([0, 0, 255]),
+)
 
 world.reset()
 camera.initialize()
